2c474e611e14bbe0afdb8518ad121a672334dfbc
stelioskon <steliosnteloss@gmail.com>
2020-05-01 02:51:18 +0300

    ready for deployment

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 5000;
+const path = require('path');
 const connectDB = require('./config/db');
+
+const app = express();
 
 // DB connection
 connectDB();
@@ -14,5 +15,18 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/post', require('./routes/api/post'));
-app.get('/', (req, res) => res.send('API running!!!!'));
+
+//Server static assets in production
+
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT} `));
