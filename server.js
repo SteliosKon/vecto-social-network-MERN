d8b4d0985594a3f564538307559a7864fe8db33f
stelioskon <steliosnteloss@gmail.com>
2020-05-01 03:00:21 +0300

    ready for deployment x2

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 const express = require('express');
-const path = require('path');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
-// DB connection
+// Connect Database
 connectDB();
 
-//init middleware
-app.use(express.json({ extended: false }));
+// Init Middleware
+app.use(express.json());
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/post', require('./routes/api/post'));
+app.use('/api/posts', require('./routes/api/posts'));
 
-//Server static assets in production
-
+// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  // set static folder
-
+  // Set static folder
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
@@ -29,4 +27,5 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT} `));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
