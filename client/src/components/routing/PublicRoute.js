ec5d392b2060e5f55ccf836008ac9e8cf5dee3e1
stelioskon <steliosnteloss@gmail.com>
2020-05-07 16:40:33 +0300

    added publicRoute component,currently not in use

---
+++ client/src/components/routing/PublicRoute.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+const PublicRoute = ({ component: Component, restricted, ...rest }) => {
+  return (
+    // restricted = false meaning public route
+    // restricted = true meaning restricted route
+    <Route
+      {...rest}
+      render={(props) =>
+        isLogin() && restricted ? (
+          <Redirect to="/dashboard" />
+        ) : (
+          <Component {...props} />
+        )
+      }
+    />
+  );
+};
+
+PublicRoute.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps)(PublicRoute);
